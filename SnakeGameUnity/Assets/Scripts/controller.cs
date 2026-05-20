135fdc9da1df09f3bbc8db19c880e2f46a3b1941
Flipzandbergen <f.zandbergen@students.uu.nl>
2026-05-20 12:20:36 +0200

    added snake

---
+++ SnakeGameUnity/Assets/Scripts/controller.cs
@@ -0,0 +1,180 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections.Generic;
+
+public class controller : MonoBehaviour
+{
+    public GameObject snake;
+    public Transform snake_transform;
+    public float time;
+    public bool W;
+    Vector3 direction = Vector3.up;
+    public Vector2 vector_input;
+    public float rl_input;
+    public float input_cooldown;
+    public bool input_on_cooldown;
+    Vector3 lastPos_0;
+    Vector3 lastPos_1;
+    Vector3 lastPos_2;
+    Vector3 lastPos_3;
+    Vector3 lastPos_4;
+    Vector3 lastPos_5;
+    Vector3 lastPos_6;
+    Vector3 lastPos_7;
+    Vector3 lastPos_8;
+    List<Vector3> last_positions = new List<Vector3>();
+    bool update;
+
+    public Transform snake_body_0;
+    public Transform snake_body_1;
+    public Transform snake_body_2;
+    public Transform snake_body_3;
+    public Transform snake_body_4;
+    public Transform snake_body_5;
+    public Transform snake_body_6;
+    public Transform snake_body_7;
+    public Transform snake_body_8;
+
+    List<Transform> snake_body = new List<Transform>();
+
+    int cycle;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        snake_body.Add(snake_body_0);
+        snake_body.Add(snake_body_1);
+        snake_body.Add(snake_body_2);
+        snake_body.Add(snake_body_3);
+        snake_body.Add(snake_body_4);
+        snake_body.Add(snake_body_5);
+        snake_body.Add(snake_body_6);
+        snake_body.Add(snake_body_7);
+        snake_body.Add(snake_body_8);
+
+        for (var i = 0; i < snake_body.Count; i++)
+        {
+            last_positions.Add(snake_transform.position);
+        }
+            
+    }
+    private InputSystem_Actions controls;
+
+    private void Awake()
+    {
+        controls = new InputSystem_Actions();
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        vector_input = controls.Player.Move.ReadValue<Vector2>();
+        rl_input = vector_input.x;
+        
+        if (rl_input == 1 && !input_on_cooldown)
+        {
+            if (direction == Vector3.up)
+            {
+                direction = Vector3.right;
+            }
+            else if (direction == Vector3.right)
+            {
+                direction = Vector3.down;
+            }
+            else if (direction == Vector3.down)
+            {
+                direction = Vector3.left;
+            }
+            else
+            {
+                direction = Vector3.up;
+            }
+            input_on_cooldown = true;
+        }
+        if (rl_input == -1 && !input_on_cooldown)
+        {
+            if (direction == Vector3.up)
+            {
+                direction = Vector3.left;
+            }
+            else if (direction == Vector3.left)
+            {
+                direction = Vector3.down;
+            }
+            else if (direction == Vector3.down)
+            {
+                direction = Vector3.right;
+            }
+            else
+            {
+                direction = Vector3.up;
+            }
+            input_on_cooldown = true;
+        }
+
+
+        if (input_on_cooldown)
+        {
+            input_cooldown += Time.deltaTime;
+            if (input_cooldown > 1)
+            {
+                input_on_cooldown = false;
+                input_cooldown = 0;
+            }
+        }
+
+            if (time > .75)
+        {
+            update = true;
+        }
+        
+
+        if (update)
+        {
+            lastPos_0 = snake_transform.position;
+            snake_transform.Translate(direction);
+            time = 0;
+            update = false;
+            input_cooldown = 0;
+            input_on_cooldown = false;
+
+
+            
+            snake_body[0].position = last_positions[0];
+            last_positions[0] = snake_transform.position;
+
+            for (var i = 1; i < snake_body.Count; i++)
+            {
+
+                snake_body[i].position = last_positions[i];
+                last_positions[i] = snake_body[i - 1].position;
+
+                
+            }
+
+            cycle =+ 1;
+/*
+            for (var i = 0; i < snake_body.Count; ++i)
+            {
+                for (var j = 0; ++i)
+                {
+                    
+                }
+            }
+*/
+
+        }
+    }
+}
