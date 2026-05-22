9bf96dae69e40f79ea594480e2c968c933503d5d
Flipzandbergen <f.zandbergen@students.uu.nl>
2026-05-22 11:34:42 +0200

    Update controller.cs

--- SnakeGameUnity/Assets/Scripts/controller.cs
+++ SnakeGameUnity/Assets/Scripts/controller.cs
@@ -13,6 +13,7 @@ public class controller : MonoBehaviour
     public float rl_input;
     public float input_cooldown;
     public bool input_on_cooldown;
+    
     Vector3 lastPos_0;
     Vector3 lastPos_1;
     Vector3 lastPos_2;
@@ -23,6 +24,18 @@ public class controller : MonoBehaviour
     Vector3 lastPos_7;
     Vector3 lastPos_8;
     List<Vector3> last_positions = new List<Vector3>();
+
+    Quaternion lastRotation_0;
+    Quaternion lastRotation_1;
+    Quaternion lastRotation_2;
+    Quaternion lastRotation_3;
+    Quaternion lastRotation_4;
+    Quaternion lastRotation_5;
+    Quaternion lastRotation_6;
+    Quaternion lastRotation_7;
+    Quaternion lastRotation_8;
+    List<Quaternion> last_rotations = new List<Quaternion>();
+
     bool update;
 
     public Transform snake_body_0;
@@ -38,6 +51,8 @@ public class controller : MonoBehaviour
     List<Transform> snake_body = new List<Transform>();
 
     int cycle;
+
+    float rotation = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -55,6 +70,7 @@ public class controller : MonoBehaviour
         for (var i = 0; i < snake_body.Count; i++)
         {
             last_positions.Add(snake_transform.position);
+            last_rotations.Add(snake_transform.rotation);
         }
             
     }
@@ -82,24 +98,41 @@ public class controller : MonoBehaviour
         time += Time.deltaTime;
         vector_input = controls.Player.Move.ReadValue<Vector2>();
         rl_input = vector_input.x;
-        
+
+
+        if (input_on_cooldown)
+        {
+            input_cooldown += Time.deltaTime;
+            if (input_cooldown > .1 && rl_input == 0)
+            {
+                input_on_cooldown = false;
+                input_cooldown = 0;
+            }
+        }
+
         if (rl_input == 1 && !input_on_cooldown)
         {
             if (direction == Vector3.up)
             {
                 direction = Vector3.right;
+                rotation = -90f;
+                Debug.Log("up to right");
             }
             else if (direction == Vector3.right)
             {
                 direction = Vector3.down;
+                rotation = 180f;
+                Debug.Log("right to up");
             }
             else if (direction == Vector3.down)
             {
                 direction = Vector3.left;
+                rotation = -270f;
             }
             else
             {
                 direction = Vector3.up;
+                rotation = 0f;
             }
             input_on_cooldown = true;
         }
@@ -108,31 +141,24 @@ public class controller : MonoBehaviour
             if (direction == Vector3.up)
             {
                 direction = Vector3.left;
+                rotation = -270f;
             }
             else if (direction == Vector3.left)
             {
                 direction = Vector3.down;
+                rotation = 180f;
             }
             else if (direction == Vector3.down)
             {
                 direction = Vector3.right;
+                rotation = -90f;
             }
             else
             {
                 direction = Vector3.up;
+                rotation = 0f;
             }
             input_on_cooldown = true;
-        }
-
-
-        if (input_on_cooldown)
-        {
-            input_cooldown += Time.deltaTime;
-            if (input_cooldown > 1)
-            {
-                input_on_cooldown = false;
-                input_cooldown = 0;
-            }
         }
 
             if (time > .75)
@@ -144,14 +170,18 @@ public class controller : MonoBehaviour
         if (update)
         {
             lastPos_0 = snake_transform.position;
-            snake_transform.Translate(direction);
+            snake_transform.rotation = Quaternion.Euler(0, 0, rotation);
+            snake_transform.Translate(new Vector3(0,1,0));
+
             time = 0;
             update = false;
             input_cooldown = 0;
-            input_on_cooldown = false;
+            if (rl_input == 0)
+            {
+                input_on_cooldown = false;
+            }
 
-
-            
+      
             snake_body[0].position = last_positions[0];
             last_positions[0] = snake_transform.position;
 
@@ -160,8 +190,7 @@ public class controller : MonoBehaviour
 
                 snake_body[i].position = last_positions[i];
                 last_positions[i] = snake_body[i - 1].position;
-
-                
+    
             }
 
             cycle =+ 1;
