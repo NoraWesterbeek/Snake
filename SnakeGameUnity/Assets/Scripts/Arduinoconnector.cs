aee87ad7e3ac669cde9e760c245ee14dd137abec
joostpastoor <j.j.pastoor@students.uu.nl>
2026-06-01 09:54:54 +0200

    Update Arduinoconnector.cs

--- SnakeGameUnity/Assets/Scripts/Arduinoconnector.cs
+++ SnakeGameUnity/Assets/Scripts/Arduinoconnector.cs
@@ -1,16 +1,54 @@
 using UnityEngine;
+using System.IO.Ports;
+using System.Threading;
 
-public class Arduinoconnector : MonoBehaviour
+public class ArduinoConnector : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private SerialPort serialPort = new SerialPort("COM8", 9600); 
+    public string decision = "";  // "L", "R" or "F"
+    private Thread readThread;
+    private bool running = true;
+
     void Start()
     {
-        
+        serialPort.Open();
+        serialPort.ReadTimeout = 100;
+
+        readThread = new Thread(ReadArduino);
+        readThread.Start();
+
+        Debug.Log("Arduino connected!");
     }
 
-    // Update is called once per frame
-    void Update()
+    // Listens to Arduino in the background
+    void ReadArduino()
     {
-        
+        while (running)
+        {
+            try
+            {
+                decision = serialPort.ReadLine().Trim();
+            }
+            catch (System.Exception) { }
+        }
+    }
+
+    // Sends 2 bytes to Arduino
+    public void SendSignals(bool left, bool right)
+    {
+        if (!serialPort.IsOpen) return;
+
+        byte[] signals = new byte[2];
+        signals[0] = left ? (byte)1 : (byte)0;
+        signals[1] = right ? (byte)1 : (byte)0;
+
+        serialPort.Write(signals, 0, 2);
+    }
+
+    void OnApplicationQuit()
+    {
+        running = false;
+        readThread?.Join();
+        if (serialPort.IsOpen) serialPort.Close();
     }
 }
